e3602f44daaf294be36416fb61782610017514dc
nasser <a.nasser@obeikan.com.sa>
2024-02-19 07:03:41 +0200

    remove unused exported Dependencies

--- lib/an_core.dart
+++ lib/an_core.dart
@@ -4,6 +4,4 @@ library an_core;
 export './src/index.dart';
 
 // ------------------ Dependencies ------------------
-export 'package:get_it/get_it.dart';
 export 'package:shared_preferences/shared_preferences.dart';
-export 'package:injectable/injectable.dart';
