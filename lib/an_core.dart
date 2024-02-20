8430b7a27732e8e97742428a1352be4dc6e10196
nasser <a.nasser@obeikan.com.sa>
2024-02-20 05:10:59 +0200

    change package_log dir

--- lib/an_core.dart
+++ lib/an_core.dart
@@ -5,4 +5,3 @@ export './src/index.dart';
 
 // ------------------ Dependencies ------------------
 export 'package:shared_preferences/shared_preferences.dart';
-export 'package:log_package/log_package.dart';
