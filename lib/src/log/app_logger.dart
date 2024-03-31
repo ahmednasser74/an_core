fb609be78dd96df8630c29b2a340eaa0782d74f6
nasser <a.nasser@obeikan.com.sa>
2024-03-31 04:02:14 +0200

    enhance logging

--- lib/src/log/app_logger.dart
+++ lib/src/log/app_logger.dart
@@ -33,11 +33,25 @@ class AppLogger extends Interceptor {
     return const JsonEncoder.withIndent('  ').convert(json);
   }
 
-  void logApi(String apiType, String path, {Object? body, Map<String?, dynamic>? response}) {
+  void logApi(String apiType, String path, {dynamic body, Object? response}) {
     if (isProd) return;
     // final now = DateTime.now();
     // final time = '${now.monthAndDay}-${now.time24Only}';
-    return debug('$apiType $path\nBody:${jsonToString(body ?? {})}\nResponse: ${response != null ? jsonToString(response) : ''} ');
+    final printResponse = response != null ? '\nResponse: ${jsonToString(response)}' : '';
+    final String printedBody = () {
+      if (body != null && body is FormData) {
+        body = body.fields.toString();
+      } else if (body != null && body is Map) {
+        body = body.toString();
+      } else if (body != null && body is String) {
+        body = body;
+      } else {
+        body = '';
+      }
+      return '\nBody:$body';
+    }();
+
+    return debug('$apiType $path$printedBody$printResponse ');
   }
 
   void showDebugger(BuildContext context) {
@@ -70,27 +84,41 @@ class AppLogger extends Interceptor {
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    final uri = options.uri;
-    final queryParam = uri.query.isEmpty ? '' : '?${uri.query}';
-    final body = options.data is FormData ? {'fields': options.data.fields.toString()} : options.data;
-    logApi('Request: ${options.method}', uri.origin + uri.path + queryParam, body: body);
-
+    try {
+      final uri = options.uri;
+      final queryParam = uri.query.isEmpty ? '' : '?${uri.query}';
+      final body = options.data is FormData ? {'fields': options.data.fields.toString()} : options.data;
+      logApi('Request: ${options.method}', uri.origin + uri.path + queryParam, body: body);
+    } catch (error) {
+      this.error('Error in logging request: $error', StackTrace.current);
+      throw Exception('Error in logging request: $error');
+    }
     super.onRequest(options, handler);
   }
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
+    logApi(
+      err.requestOptions.method,
+      err.requestOptions.path,
+      body: err.requestOptions.data,
+      response: err.response?.data,
+    );
     error(err.message, err.stackTrace);
     super.onError(err, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    final uri = response.requestOptions.uri;
-    final method = response.requestOptions.method;
+    try {
+      final uri = response.requestOptions.uri;
+      final method = response.requestOptions.method;
 
-    logApi(method, uri.toString(), response: response.data, body: response.requestOptions.data);
-
+      logApi(method, uri.toString(), response: response.data, body: response.requestOptions.data);
+    } catch (error) {
+      this.error('Error in logging response: $error ', StackTrace.current);
+      throw Exception('Error in logging response: $error');
+    }
     // final responseHeaders = <String, String>{};
     // response.headers.forEach((k, list) => responseHeaders[k] = list.toString());
     // _printMapAsTable(responseHeaders, header: 'Headers');
