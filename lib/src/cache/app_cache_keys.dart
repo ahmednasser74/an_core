e5e7ac705ff6dfe2049ff66032ea3eb6c2c70033
Ahmed Mohamed Nasser <50350016+ahmednasser74@users.noreply.github.com>
2025-04-21 18:10:34 +0400

    modify app cache keys

--- lib/src/cache/app_cache_keys.dart
+++ lib/src/cache/app_cache_keys.dart
@@ -1,5 +1,7 @@
 abstract class AppCacheKeys {
   static const String token = "token";
+  static const String accessToken = "accessToken";
+  static const String refreshToken = "refreshToken";
   static const String user = 'user';
   static const String fcmToken = 'fcmToken';
   static const String themeMode = 'themeMode';
